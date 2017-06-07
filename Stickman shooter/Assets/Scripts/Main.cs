e848039bee11d437204bf9191c837f5cb8cd36fe
ValeryChvach <valerychvach@gmail.com>
2017-06-08 02:25:54 +0300

    no message

---
+++ Stickman shooter/Assets/Scripts/Main.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    private void Start()
+    {
+        GameObject.Find("Canvas_game").SetActive(false); // Close UI arrow
+    }
+
+    public void GoMultiplayer()
+    {
+        //Multiplayer
+
+        GameObject.Find("Canvas_menu").SetActive(false); // Close UI menu
+    }
+
+    public void GoAI()
+    {
+        //AI
+        GameObject.Find("Canvas_menu").SetActive(false); // Close UI menu
+
+        gameObject.AddComponent(typeof(PlayGame));
+    }
+}
---
+++ Stickman shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public void MoveUp()
+    {
+        Debug.Log("Up");
+    }
+
+    public void MoveDown()
+    {
+        Debug.Log("Down");
+    }
+}
